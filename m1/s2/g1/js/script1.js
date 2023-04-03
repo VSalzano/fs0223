3700e6fd8c8461146d043949b11aac3e0096723e
VSalzano <vincenzo.salzano@outlook.com>
2023-04-03 15:32:02 +0200

    fine

--- m1/s2/g1/js/script1.js
+++ m1/s2/g1/js/script1.js
@@ -1,7 +1,7 @@
 if (areaRettangolo >= 100) {
     document.getElementById("testo1").innerHTML = "Questo rettangolo è molto grande";
 }else if (areaRettangolo <= 30) {
-    document.getElementById("testo1").innerHTML = "Questo rettangolo è molto piccolo"
+    document.getElementById("testo1").innerHTML = "Questo rettangolo è molto piccolo";
 }else {
-    document.getElementById("testo1").innerHTML = "Questo rettangolo è di medie dimensioni"
+    document.getElementById("testo1").innerHTML = "Questo rettangolo è di medie dimensioni";
 }
