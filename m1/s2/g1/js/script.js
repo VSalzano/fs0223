d44ae54920e317623f8d64ff2386cc07d4ba8ee6
VSalzano <vincenzo.salzano@outlook.com>
2023-04-03 15:06:31 +0200

    second commit

---
+++ m1/s2/g1/js/script.js
@@ -0,0 +1,8 @@
+var base = 3;
+var altezza = 5;
+var areaRettangolo = base * altezza;
+
+alert ("Questo script calcola l'area del rettangolo");
+
+document.getElementById('testo').innerHTML = "L'area del rettangolo è di " + areaRettangolo + " centimetri quadrati";
+
