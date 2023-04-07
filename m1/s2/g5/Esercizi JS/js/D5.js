f03a2fcd0e11fb25045267ae8a473f80d3a38d75
VSalzano <vincenzo.salzano@outlook.com>
2023-04-07 10:53:53 +0200

    fino all'8

--- m1/s2/g5/Esercizi JS/js/D5.js
+++ m1/s2/g5/Esercizi JS/js/D5.js
@@ -11,17 +11,27 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
+console.log(pets[0],pets[1],pets[2],pets[3]);
+
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+
+pets.sort();
+
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
+console.log(pets.reverse());
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+
+pets.push(pets.shift());
+console.log(pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -47,20 +57,57 @@ const cars = [
   },
 ]
 
+cars.forEach(function(license){
+  license.licensePlate = Math.random()
+});
+
+console.log(cars);
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+
+const newCar = {};
+newCar.brand = 'Toyota';
+newCar.model = 'Aygo';
+newCar.color = 'red';
+newCar.licensePlate = Math.random();
+newCar.trims = ['titanium', 'st', 'active'];
+
+cars.push(newCar);
+
+for(let i = 0; i < cars.length; i++){
+  cars[i].trims.pop();
+}
+
+console.log(cars);
+
+
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = []
 
+for(let p = 0; p < cars.length; p++){
+  justTrims.push(cars[p].trims[0]);
+}
+
+console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+for(let j = 0; j < cars.length; j++){
+  if(cars[j].color.startsWith('b')){
+    console.log('Fizz');
+  }else{
+    console.log('Buzz');
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
