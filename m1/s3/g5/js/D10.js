9807e59b7da14e4d6720969604b196aa5bf9dfd9
VSalzano <vincenzo.salzano@outlook.com>
2023-04-14 19:39:20 +0200

    29 corretto ancora

--- m1/s3/g5/js/D10.js
+++ m1/s3/g5/js/D10.js
@@ -610,7 +610,7 @@ tree(3)
 function isItPrime(num) {
   
 
-  if(num == 0 || num == 1 || num % 2 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0){
+  if(num == 0 || num == 1 || num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || num % 17 == 0 ){
     return false; 
   }
 
@@ -625,4 +625,4 @@ function isItPrime(num) {
 
 }
 
-console.log(isItPrime(37));
+console.log(isItPrime(1039));
