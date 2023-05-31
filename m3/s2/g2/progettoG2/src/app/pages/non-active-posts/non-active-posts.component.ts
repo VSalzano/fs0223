9ba8f5fa439c59e95e4cfdc90f3087429173045c
VSalzano <vincenzo.salzano@outlook.com>
2023-05-31 09:17:01 +0200

    finito

--- m3/s2/g2/progettoG2/src/app/pages/non-active-posts/non-active-posts.component.ts
+++ m3/s2/g2/progettoG2/src/app/pages/non-active-posts/non-active-posts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Posts } from 'src/app/Models/posts';
+import { PostsService } from 'src/app/posts.service';
 
 @Component({
   selector: 'app-non-active-posts',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class NonActivePostsComponent {
 
+  nonActivePostsArr:Posts[] = [];
+
+  constructor(private postsSvc: PostsService) {
+    this.postsSvc.getPosts()
+    .then((activePosts:Posts[]) => this.nonActivePostsArr = activePosts.filter(el => el.active == false));
+  }
 }
