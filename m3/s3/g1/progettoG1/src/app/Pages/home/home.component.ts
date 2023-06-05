0ebd8930a019f561826c0af4fe34cf01b10ab13d
VSalzano <vincenzo.salzano@outlook.com>
2023-06-05 16:03:39 +0200

    elimina e like

--- m3/s3/g1/progettoG1/src/app/Pages/home/home.component.ts
+++ m3/s3/g1/progettoG1/src/app/Pages/home/home.component.ts
@@ -10,11 +10,22 @@ import { Photo } from 'src/app/Models/photo';
 export class HomeComponent implements OnInit {
   constructor(private photosSvc: PhotosService) {}
   photosArr: Photo[] = [];
+  likedPhotos: Photo[] = [];
 
   ngOnInit(): void {
     this.photosSvc.get().subscribe((res) => {
       this.photosArr = res.filter((photo) => photo.albumId == 1);
       console.log(this.photosArr);
     });
+  }
+
+  deletePhoto(photo: Photo) {
+    let index = this.photosArr.indexOf(photo);
+    this.photosArr.splice(index, 1);
+  }
+
+  like(photo: Photo) {
+    this.likedPhotos.push(photo);
+    console.log(this.likedPhotos.length);
   }
 }
