7c82add5d6ef564775cb7a5ba1c21fd0d533ccd7
VSalzano <vincenzo.salzano@outlook.com>
2023-06-08 16:42:19 +0200

    inizio login

--- m3/s3/g4/progettoG4/src/app/Pages/auth/auth.service.ts
+++ m3/s3/g4/progettoG4/src/app/Pages/auth/auth.service.ts
@@ -2,11 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Iregister } from './Models/iregister';
 import { environment } from 'src/environments/environment.development';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject } from 'rxjs';
+import { IAccessData } from './Models/iaccess-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
+  private authSubject = new BehaviorSubject<null | IAccessData>(null);
+
   constructor(private http: HttpClient) {}
 
   register(user: Iregister) {
