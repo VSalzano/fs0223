d99386aa0948f4ed6af42218a9f3dbe2f55e2d49
VSalzano <vincenzo.salzano@outlook.com>
2023-06-08 16:58:52 +0200

    login

--- m3/s3/g4/progettoG4/src/app/Pages/auth/auth.service.ts
+++ m3/s3/g4/progettoG4/src/app/Pages/auth/auth.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 import { Iregister } from './Models/iregister';
 import { environment } from 'src/environments/environment.development';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, tap } from 'rxjs';
 import { IAccessData } from './Models/iaccess-data';
+import { LoginUser } from './Models/login-user';
 
 @Injectable({
   providedIn: 'root',
@@ -14,9 +15,18 @@ export class AuthService {
 
   private authSubject = new BehaviorSubject<null | IAccessData>(null);
 
+  user$ = this.authSubject.asObservable();
+  isLoggedIn$ = this.user$.pipe(map((data) => Boolean(data)));
+
   constructor(private http: HttpClient) {}
 
   register(user: Iregister) {
     return this.http.post<Iregister>(environment.registerUrl, user);
+  }
+
+  login(user: LoginUser) {
+    return this.http
+      .post<IAccessData>(environment.logUrl, user)
+      .pipe(tap((data) => {}));
   }
 }
